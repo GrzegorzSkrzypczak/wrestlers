ff380aa4ce38460a47f7bdfe1df5c3eed09c045b
Grzegorz <rowingteam.grzegorz@gmail.com>
2021-02-20 15:33:09 +0100

    Added remove wrestler and referee method

--- src/pl/grzegorz/wrestlers/app/WrestlersController.java
+++ src/pl/grzegorz/wrestlers/app/WrestlersController.java
@@ -22,15 +22,15 @@ public class WrestlersController {
     private WrestlersLibrary wrestlersLibrary;
     private FileMenager fileMenager;
 
-     WrestlersController() {
+    WrestlersController() {
         fileMenager = new FileMenagerBuilder(printer, dataReader).build();
         try {
             wrestlersLibrary = fileMenager.importData();
             printer.printLine("Zaimportowano dane z pliku.");
-        }catch (DataImportException | InvalidDataException e) {
+        } catch (DataImportException | InvalidDataException e) {
             printer.printLine(e.getMessage());
             printer.printLine("Zainicjowano nową bazę!");
-            wrestlersLibrary =  new WrestlersLibrary();
+            wrestlersLibrary = new WrestlersLibrary();
         }
     }
 
@@ -53,6 +53,12 @@ public class WrestlersController {
                 case PRINT_REFEREE:
                     printReferees();
                     break;
+                case DELATE_WRESTLER:
+                    delateWrestler();
+                    break;
+                case DELATE_REFEREE:
+                    delateReferee();
+                    break;
                 case EXIT:
                     exit();
                     break;
@@ -62,11 +68,12 @@ public class WrestlersController {
         } while (option != Options.EXIT);
     }
 
+
     private Options getOption() {
         boolean optionOk = false;
         Options options = null;
 
-        while (!optionOk){
+        while (!optionOk) {
             try {
                 options = Options.createFromInt(dataReader.getInt());
                 optionOk = true;
@@ -81,7 +88,7 @@ public class WrestlersController {
 
     private void printOptions() {
         printer.printLine("Wybierz opcję: ");
-        for(Options option: Options.values()){
+        for (Options option : Options.values()) {
             printer.printLine(option.toString());
         }
     }
@@ -97,16 +104,39 @@ public class WrestlersController {
         }
     }
 
+    private void delateWrestler() {
+        try {
+        Wrestlers wrestlers = dataReader.readAndCreateWrestler();
+        if(wrestlersLibrary.removeEmployee(wrestlers))
+            printer.printLine("Usunięto wrestlera");
+        else
+            printer.printLine("Brak takiego wrestlera!");
+        } catch(InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć wrestlera, niepoprawne dane!");
+        }
+    }
+
     private void addReferee() {
         try {
             Referees referees = dataReader.readAndCreateReferees();
             wrestlersLibrary.addEmployees(referees);
         } catch (InputMismatchException e) {
             printer.printLine("Nie udało się dodać sędziego, spróbuj ponownie!");
-        }  catch (ArrayIndexOutOfBoundsException e) {
-        printer.printLine("Brak miejsca na zapis sędziego!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            printer.printLine("Brak miejsca na zapis sędziego!");
+        }
     }
 
+    private void delateReferee() {
+        try {
+            Referees referees = dataReader.readAndCreateReferees();
+            if (wrestlersLibrary.removeEmployee(referees))
+                printer.printLine("Usunięto sędziego");
+            else
+                printer.printLine("Brak takiego sędziego!");
+        } catch(InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć sędziego, niepoprawne dane!");
+        }
     }
 
     private void printWrestlers() {
@@ -119,24 +149,27 @@ public class WrestlersController {
         printer.printReferee(referees);
     }
 
+
     private void exit() {
-         try {
-         fileMenager.exportData(wrestlersLibrary);
-         printer.printLine("Export danych do pliku zakończony powodzeniem.");
-         } catch (DataExportException e) {
-             printer.printLine(e.getMessage());
-         }
+        try {
+            fileMenager.exportData(wrestlersLibrary);
+            printer.printLine("Export danych do pliku zakończony powodzeniem.");
+        } catch (DataExportException e) {
+            printer.printLine(e.getMessage());
+        }
         printer.printLine("Koniec programu.");
         dataReader.close();
     }
 
-   private enum Options {
+    private enum Options {
 
         EXIT(0, "Wyjście z programu"),
         ADD_WRESTLER(1, "Dodaj wrestlera"),
-        ADD_REFEREE(2,"Dodaj sędziego"),
+        ADD_REFEREE(2, "Dodaj sędziego"),
         PRINT_WRESTLER(3, "Wyświetl wrestlerów"),
-        PRINT_REFEREE(4, "WYświetl sędziów");
+        PRINT_REFEREE(4, "WYświetl sędziów"),
+        DELATE_WRESTLER(5, "Usuń Wrestlera"),
+        DELATE_REFEREE(6, "Usuń sędziego");
 
         private int value;
         private String description;
--- src/pl/grzegorz/wrestlers/model/WrestlersLibrary.java
+++ src/pl/grzegorz/wrestlers/model/WrestlersLibrary.java
@@ -1,12 +1,13 @@
 package pl.grzegorz.wrestlers.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class WrestlersLibrary implements Serializable {
 
-    private static final int MAX_SPACE = 2000;
-    private Company[] company = new Company[MAX_SPACE];
-    private int companySpace;
+    private static final int CAPACITY = 1;
+    private Company[] company = new Company[CAPACITY];
+    private int companySpace = 0;
 
     public Company[] getCompany() {
         Company[] result = new Company[companySpace];
@@ -17,10 +18,32 @@ public class WrestlersLibrary implements Serializable {
     }
 
     public void addEmployees(Company employees) {
-        if (companySpace >= MAX_SPACE) {
-            throw new ArrayIndexOutOfBoundsException("No more space in database to save wrestler or referee!" + MAX_SPACE);
+        if (companySpace == company.length) {
+            company = Arrays.copyOf(company, company.length * 2);
         }
         company[companySpace] = employees;
         companySpace++;
+    }
+
+    public boolean removeEmployee(Company comp) {
+        final int notFound = -1;
+        int found = notFound;
+        int i = 0;
+
+        while(i < companySpace && found == notFound) {
+            if(comp.equals(company[i])) {
+                found = i;
+            } else {
+                i++;
+            }
+        }
+
+        if(found != notFound){
+            System.arraycopy(company, found +1, company, found, company.length - found -1);
+            companySpace--;
+            company[companySpace] = null;
+        }
+
+        return found != notFound;
     }
 }
